f51900e9fa31bdf5b21e085c0dec38270a7d46d6
Darshan7573 <darshu7573@gmail.com>
2024-09-28 00:30:26 +0530

    Basic vehicle movement api  backend

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,16 @@ app.use(express.json())
 app.use(cors())
 
 
+const vehicleData = [
+    { "latitude": 17.385044, "longitude": 78.486671, "timestamp": "2024-07-20T10:00:00Z" },
+    { "latitude": 17.385045, "longitude": 78.486672, "timestamp": "2024-07-20T10:00:05Z" },
+    { "latitude": 17.385046, "longitude": 78.486673, "timestamp": "2024-07-20T10:00:10Z" },
+]
+
+app.get('/api/vehicle-data', (req, res) => {
+    res.json(vehicleData)
+})
+
 app.listen(3000, () => {
     console.log('Server Connected')
 })
